011ad774692246cc9b8bdf00267cc058176eac11
Shubham Upadhyay <shubhamup47@gmail.com>
2023-06-16 18:28:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species)
+	{
+		this._species = species;
+	}
+	setSpecies(species)
+	{
+		this._species = species;
+	}
+	getSpecies()
+	{
+		return this._species;
+	}
+	makeSound()
+	{
+		console.log("sound");
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	bark()
+	{
+		console.log("woof");
+	}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	purr()
+	{
+		console.log("purr");
+	}
+}
+
+let cat = new Cat("Wild cat");
+cat.species;
+cat.makeSound();
+cat.purr();
+
+let dog = new Dog("German");
+dog.species;
+dog.makeSound();
+dog.bark();
 
 // Do not change the code below this line
 window.Animal = Animal;
